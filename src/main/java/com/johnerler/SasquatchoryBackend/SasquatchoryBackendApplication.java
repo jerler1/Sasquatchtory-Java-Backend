e62cfc0277750137fa7a451edfc61038a61bc5a9
Bunks <busk_1@yahoo.com>
2021-07-14 21:41:19 -0400

    initial commit

---
+++ src/main/java/com/johnerler/SasquatchoryBackend/SasquatchoryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.johnerler.SasquatchoryBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SasquatchoryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SasquatchoryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/johnerler/SasquatchoryBackend/SasquatchoryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnerler.SasquatchoryBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SasquatchoryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
