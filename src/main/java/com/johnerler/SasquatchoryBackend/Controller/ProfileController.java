6b4f40c9c4c7ed249d66cad9db5f8585a439a740
Bunks <busk_1@yahoo.com>
2021-07-28 00:35:48 -0400

    added all the routes for the controller

--- src/main/java/com/johnerler/SasquatchoryBackend/Controller/ProfileController.java
+++ src/main/java/com/johnerler/SasquatchoryBackend/Controller/ProfileController.java
@@ -1,7 +1,49 @@
 package com.johnerler.SasquatchoryBackend.Controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.johnerler.SasquatchoryBackend.model.Profile;
+import com.johnerler.SasquatchoryBackend.repository.ProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/profile")
 public class ProfileController {
+
+    @Autowired
+    ProfileRepository repo;
+
+    @GetMapping("/")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Profile> getAllProfiles() {
+        return repo.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Profile getProfileById(@PathVariable int id) {
+        Optional<Profile> returnVal = repo.findById(id);
+        return returnVal.orElse(null);
+    }
+
+    @PostMapping("/")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Profile addProfile(@RequestBody Profile profile) {
+        return repo.save(profile);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateProfile(@RequestBody Profile profile) {
+        repo.save(profile);
+    }
+
+    @DeleteMapping("/{id")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProfile(@PathVariable int id) {
+        repo.deleteById(id);
+    }
 }
